bdef1261539a57844b673822962e990a626f29e4
FrancoDujanoff <fdujanoff@gmail.com>
2023-04-13 10:05:57 -0300

    Se agrego que cuando se elige el color del producto, se cambia la imagen

---
+++ js/script_index.js
@@ -0,0 +1,47 @@
+function cambiarImagenSilla1(i) {
+    document.getElementById("imagen_silla_1").src = i;
+}
+
+function cambiarImagenSilla2(i) {
+    document.getElementById("imagen_silla_2").src = i;
+}
+
+function cambiarImagenSilla3(i) {
+    document.getElementById("imagen_silla_3").src = i;
+}
+
+function cambiarImagenMesa1(i) {
+    document.getElementById("imagen_mesa_1").src = i;
+}
+
+function cambiarImagenMesa2(i) {
+    document.getElementById("imagen_mesa_2").src = i;
+}
+
+function cambiarImagenMesa3(i) {
+    document.getElementById("imagen_mesa_3").src = i;
+}
+
+function cambiarImagenOrganizador1(i) {
+    document.getElementById("imagen_organizador_1").src = i;
+}
+
+function cambiarImagenOrganizador2(i) {
+    document.getElementById("imagen_organizador_2").src = i;
+}
+
+function cambiarImagenOrganizador3(i) {
+    document.getElementById("imagen_organizador_3").src = i;
+}
+
+function cambiarImagenArmario1(i) {
+    document.getElementById("imagen_armario_1").src = i;
+}
+
+function cambiarImagenArmario2(i) {
+    document.getElementById("imagen_armario_2").src = i;
+}
+
+function cambiarImagenArmario3(i) {
+    document.getElementById("imagen_armario_3").src = i;
+}
