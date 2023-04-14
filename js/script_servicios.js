7521430242fcf59b46c1f87b8da026b1d1e9a15c
FrancoDujanoff <fdujanoff@gmail.com>
2023-04-14 11:51:10 -0300

    Terminada pagina servicios. Agregado de lectura de formularios

--- js/script_servicios.js
+++ js/script_servicios.js
@@ -1,31 +1,47 @@
-$(document).ready(function() {
-    $(".btn-seccion1").click(function(){
-        if($(".seccion1").is(":visible")){
-            $(".seccion1").slideUp();
-        } else {
-            $(".seccion1").slideDown();
-            $(".seccion2, .seccion3").slideUp();
-        }
-    });
-    
-    $(".btn-seccion2").click(function(){
-        if($(".seccion2").is(":visible")){
-            $(".seccion2").slideUp();
-        } else {
-            $(".seccion2").slideDown();
-            $(".seccion1, .seccion3").slideUp();
-        }
-    });
-    
-    $(".btn-seccion3").click(function(){
-        if($(".seccion3").is(":visible")){
-            $(".seccion3").slideUp();
-        } else {
-            $(".seccion3").slideDown();
-            $(".seccion1, .seccion2").slideUp();
-        }
-    });
-});
 
-//Sacado de google con el objetivo de lograr la animacion
+//Formulario de Reclamos
+let formulario_reclamo = document.getElementById("formulario_reclamos");
 
+formulario_reclamo.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let nombre_r = document.getElementById('nombre_reclamo').value;
+    let apellido_r = document.getElementById('apellido_reclamo').value;
+    let email_r = document.getElementById("email_reclamo").value;
+    let numero_r = document.getElementById("numero_reclamo").value;
+
+    let reclamo = {
+        nombre: nombre_r,
+        apellido: apellido_r,
+        email: email_r,
+        numero: numero_r
+    };
+
+    console.log(reclamo);
+
+})
+
+//Formulario de Cancelacion
+let formulario_cancelacion = document.getElementById("formulario_cancelacion");
+
+formulario_cancelacion.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let nombre_r = document.getElementById('nombre_cancelacion').value;
+    let apellido_r = document.getElementById('apellido_cancelacion').value;
+    let email_r = document.getElementById("email_cancelacion").value;
+    let numero_r = document.getElementById("numero_cancelacion").value;
+    let numerop_r = document.getElementById("numero_reclamo_cancelacion").value;
+    let comentario_r = document.getElementById("comentario_cancelacion").value;
+
+
+    let cancelacion = {
+        nombre: nombre_r,
+        apellido: apellido_r,
+        email: email_r,
+        numero: numero_r,
+        numero_pedido: numerop_r,
+        comentario: comentario_r
+    };
+
+    console.log(cancelacion);
+
+})
