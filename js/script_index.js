af9f78823c64abb4424bf48e14072eb7276dfaf3
FrancoDujanoff <fdujanoff@gmail.com>
2023-04-14 12:23:54 -0300

    Comienzo carrito-agregado control de formularios

--- js/script_index.js
+++ js/script_index.js
@@ -45,3 +45,89 @@ function cambiarImagenArmario2(i) {
 function cambiarImagenArmario3(i) {
     document.getElementById("imagen_armario_3").src = i;
 }
+
+let pedido_silla_1 = document.getElementById("formulario_silla_1");
+let pedido_silla_2 = document.getElementById("formulario_silla_2");
+let pedido_silla_3 = document.getElementById("formulario_silla_3");
+let pedido_mesa_1 = document.getElementById("formulario_mesa_1");
+let pedido_mesa_2 = document.getElementById("formulario_mesa_2");
+let pedido_mesa_3 = document.getElementById("formulario_mesa_3");
+let pedido_organizador_1 = document.getElementById("formulario_organizador_1");
+let pedido_organizador_2 = document.getElementById("formulario_organizador_2");
+let pedido_organizador_3 = document.getElementById("formulario_organizador_3");
+let pedido_armario_1 = document.getElementById("formulario_armario_1");
+let pedido_armario_2 = document.getElementById("formulario_armario_2");
+let pedido_armario_3 = document.getElementById("formulario_armario_3");
+
+pedido_silla_1.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_silla_1 = document.getElementById('cantidad_silla_1').value;
+    let color_silla_1 = document.getElementById('color_silla_1').value;
+
+})
+pedido_silla_2.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_silla_2 = document.getElementById('cantidad_silla_2').value;
+    let color_silla_2 = document.getElementById('color_silla_2').value;
+
+})
+pedido_silla_3.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_silla_3 = document.getElementById('cantidad_silla_3').value;
+    let color_silla_3 = document.getElementById('color_silla_3').value;
+
+})
+pedido_mesa_1.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_mesa_1 = document.getElementById('cantidad_mesa_1').value;
+    let color_mesa_1 = document.getElementById('color_mesa_1').value;
+
+})
+pedido_mesa_2.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_mesa_2 = document.getElementById('cantidad_mesa_2').value;
+    let color_mesa_2 = document.getElementById('color_mesa_2').value;
+
+})
+pedido_mesa_3.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_mesa_3 = document.getElementById('cantidad_mesa_3').value;
+    let color_mesa_3 = document.getElementById('color_mesa_3').value;
+
+})
+pedido_armario_1.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_armario_1 = document.getElementById('cantidad_armario_1').value;
+    let color_armario_1 = document.getElementById('color_armario_1').value;
+
+})
+pedido_armario_2.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_armario_2 = document.getElementById('cantidad_armario_2').value;
+    let color_armario_2 = document.getElementById('color_armario_2').value;
+
+})
+pedido_armario_3.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_armario_3 = document.getElementById('cantidad_armario_3').value;
+    let color_armario_3 = document.getElementById('color_armario_3').value;
+
+})
+pedido_organizador_1.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_organizador_1 = document.getElementById('cantidad_organizador_1').value;
+    let color_organizador_1 = document.getElementById('color_organizador_1').value;
+
+})
+pedido_organizador_2.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_organizador_2 = document.getElementById('cantidad_organizador_2').value;
+    let color_organizador_2 = document.getElementById('color_organizador_2').value;
+
+})
+pedido_organizador_3.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let cantidad_organizador_3 = document.getElementById('cantidad_organizador_3').value;
+    let color_organizador_3 = document.getElementById('color_organizador_3').value;
+
+})
